c8937877009afd63c4ca6d978b66dfd3ed66e145
renasboy <renasboy@gmail.com>
2015-12-24 16:22:57 +0100

    Updated model

--- src/api/models.py
+++ src/api/models.py
@@ -5,7 +5,7 @@ class Quality(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=30)
-    active = models.NullBooleanField()
+    active = models.BooleanField(default=False)
 
 
 class Person(models.Model):
@@ -14,17 +14,17 @@ class Person(models.Model):
     name = models.CharField(max_length=80)
     fb = models.CharField(max_length=80)
     img = models.URLField()
-    qualities = models.ManyToManyField(
-        'Quality',
-        through='Judgement',
-        through_fields=('judged', 'quality')
-    )
 
 
 class Judgement(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     judged = models.ForeignKey('Person')
     judge = models.ForeignKey('Person', related_name='judgements')
+    why = models.CharField(max_length=255)
+
+
+class JudgementQuality(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    judgement = models.ForeignKey('Judgement')
     quality = models.ForeignKey('Quality')
     score = models.PositiveSmallIntegerField()
-    why = models.CharField(max_length=255)
