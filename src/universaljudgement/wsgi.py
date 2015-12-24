d28263771cd76c5c843a0a659e37a956e3a40f3e
renasboy <renasboy@gmail.com>
2015-12-24 22:02:16 +0100

    Reconfiguring the app

--- src/universaljudgement/wsgi.py
+++ src/universaljudgement/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for universaljudgement project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "universaljudgement.settings")
 
-application = get_wsgi_application()
+def application(env, start):
+    os.environ['SECRET_KEY'] = env['SECRET_KEY']
+    app = get_wsgi_application()
+    return app(env, start)
