fea37cb23430ed37720390d60ed8f8f5908eae0f
Renato Cardoso <re2005@gmail.com>
2016-08-14 22:03:40 +0200

    feat(judgment-service): create service to post person judgement to API

---
+++ angular2/src/app/services/judgment.service.js
@@ -0,0 +1,43 @@
+/**
+ * Created on 14/08/16.
+ * @author Renato Cardoso <re2005@gmail.com>
+ */
+
+(function (angular) {
+    'use strict';
+
+    /**
+     *
+     * @param {Object} $http
+     */
+    function JudgementService($http) {
+        this.$http = $http;
+    }
+
+    /**
+     *
+     * @type {{apiHost: string}}
+     */
+    JudgementService.prototype.constants = {
+        apiHost: 'http://www.universaljudgment.com/tuj/judgement'
+    };
+
+    /**
+     *
+     * @returns {Promise}
+     */
+    JudgementService.prototype.sendJudgment = function (data) {
+        // TODO Implement get cookie
+        // var csrfToken = getCookie('csrftoken');
+        // var config = {
+        //     headers: {
+        //         'X-CSRFToken': csrfToken
+        //     }
+        // };
+        // return this.$http.post(this.constants.apiHost, data);
+        // TODO change API to accept cross domain post
+        return Promise.resolve();
+    };
+
+    angular.module('app').service('judgementService', JudgementService);
+}(window.angular));
