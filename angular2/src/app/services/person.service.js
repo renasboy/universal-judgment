bdf9d6b3261b9330bb758d521718ed85af9d7449
Renato Cardoso <re2005@gmail.com>
2016-08-14 02:47:47 +0200

    feat(person-service): access person API return promisse

---
+++ angular2/src/app/services/person.service.js
@@ -0,0 +1,29 @@
+/**
+ * Created on 14/08/16.
+ * @author Renato Cardoso <re2005@gmail.com>
+ */
+
+(function (angular) {
+    'use strict';
+
+    /**
+     *
+     * @param {Object} $http
+     */
+    function PersonService($http) {
+        this.$http = $http;
+    };
+
+    PersonService.prototype.constants = {
+        apiHost: 'http://www.universaljudgment.com/tuj/person/'
+    };
+
+    PersonService.prototype.getPerson = function (id) {
+        return this.$http({
+            url: this.constants.apiHost+id
+        });
+    };
+
+
+    angular.module('app').service('personService', PersonService);
+}(window.angular));
