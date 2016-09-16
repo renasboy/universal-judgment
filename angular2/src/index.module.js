4766261cf775336b4182bca2e7cabdddb275a300
Renato Cardoso <re2005@gmail.com>
2016-09-16 21:51:45 +0200

    refactor(index-config): remove

--- angular2/src/index.module.js
+++ angular2/src/index.module.js
@@ -2,6 +2,18 @@
 
     'use strict';
 
-    angular.module('app', ['ui.router']);
+
+    /**
+     *
+     * @param FacebookProvider
+     */
+    function config(FacebookProvider) {
+        FacebookProvider.init('993028097483020');
+    }
+
+    angular.module('app', [
+        'ui.router',
+        'facebook'
+    ]).config(config);
 
 })(window.angular);
