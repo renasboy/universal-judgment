a5673cd3a4a3412e573eac503e05a05fe1685d30
renasboy <renasboy@gmail.com>
2016-10-26 18:39:57 +0200

    judgment instead of judgment in the api

--- src/universaljudgement/urls.py
+++ src/universaljudgement/urls.py
@@ -8,7 +8,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^admin/?', admin.site.urls),
     url(r'^qualities/?$', views.Qualities.as_view()),
-    url(r'^judgement/?$', views.Judgement.as_view()),
+    url(r'^judgment/?$', views.Judgement.as_view()),
     url(r'^people/?$', views.People.as_view()),
     url(r'^person/(?P<id>[0-9]+)/?$', views.Person.as_view()),
     url(r'^person', views.Person.as_view()),
