2efb102242c41c704a7146368eead3f6c37baa84
renasboy <renasboy@gmail.com>
2016-10-27 02:44:41 +0200

    Translation it, en

--- angular2/src/index.js
+++ angular2/src/index.js
@@ -1,8 +1,41 @@
 (function (angular) {
     'use strict';
 
-    function config(FacebookProvider) {
+    function config(FacebookProvider, $translateProvider) {
         FacebookProvider.init('993028097483020');
+
+        $translateProvider.translations('en', {
+            JUDGE_NOW: 'Judge Now',
+            SUBMIT: 'Submit',
+            DONE: 'Done',
+            NEW_SCORE: 'New Score',
+            WHY: 'Why?',
+            APPERANCE: 'Apperance',
+            INTELLIGENCE: 'Intelligence',
+            HONESTY: 'Honesty',
+            PROFESSIONALISM: 'Professionalism',
+        });
+        $translateProvider.translations('it', {
+            JUDGE_NOW: 'Giudica',
+            SUBMIT: 'Invia',
+            DONE: 'Fatto',
+            NEW_SCORE: 'Nuovo punteggio',
+            WHY: 'Perche?',
+            APPERANCE: 'Apparenza',
+            INTELLIGENCE: 'Intelligenza',
+            HONESTY: 'Onesta',
+            PROFESSIONALISM: 'Professionalismo',
+        });
+        var langs = ['en', 'it'];
+        var lang = (navigator.userLanguage 
+                    || navigator.language
+                    || navigator.browserLanguage
+                    || navigator.systemLanguage).split('-')[0]
+        if (langs.indexOf(lang) === -1) {
+            lang = langs[0];
+        }
+        $translateProvider.preferredLanguage(lang); 
+        //$translateProvider.useSanitizeValueStrategy('sanitize');
     }
 
     // TODO [Renato] move to gulp config
@@ -10,6 +43,7 @@
 
     angular.module('app', [
         'ui.router',
-        'facebook'
+        'facebook',
+        'pascalprecht.translate'
     ]).config(config);
 }(window.angular));
