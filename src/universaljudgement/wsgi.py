c137c251c6b1c9f9a277c87e7bd304d95b20eb48
renasboy <renasboy@gmail.com>
2017-02-16 20:22:51 +0100

    Using postgres instead of sqlite

--- src/universaljudgement/wsgi.py
+++ src/universaljudgement/wsgi.py
@@ -8,5 +8,11 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "universaljudgement.settings")
 
 def application(env, start):
     os.environ['SECRET_KEY'] = env['SECRET_KEY']
+    os.environ['DB_HOST'] = env['DB_HOST']
+    os.environ['DB_PORT'] = env['DB_PORT']
+    os.environ['DB_NAME'] = env['DB_NAME']
+    os.environ['DB_USER'] = env['DB_USER']
+    os.environ['DB_PASS'] = env['DB_PASS']
+    
     app = get_wsgi_application()
     return app(env, start)
