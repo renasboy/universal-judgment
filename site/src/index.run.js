d8b5d59b64ff3384f981edbb391944378db4b822
Renato Cardoso <re2005@gmail.com>
2017-02-17 22:37:03 +0100

    feat(index.run): add interceptor

--- site/src/index.run.js
+++ site/src/index.run.js
@@ -1,7 +1,12 @@
 (function (angular) {
     'use strict';
 
-    function run($rootScope, authService, Facebook) {
+    var _cache = {};
+
+    function run($rootScope, authService, Facebook, $cacheFactory) {
+
+        _cache = $cacheFactory;
+
         $rootScope.$watch(
             function () {
                 return Facebook.isReady();
@@ -16,5 +21,41 @@
         );
     }
 
-    angular.module('app').run(run);
+
+    function cleanCache(isPost) {
+        if (isPost) {
+            var checkCache = _cache.info();
+            angular.forEach(checkCache, function (obj, key) {
+                _cache.get(key).removeAll();
+            });
+        }
+    }
+
+    function testInterceptor() {
+        return {
+            request: function (config) {
+                return config;
+            },
+
+            requestError: function (config) {
+                return config;
+            },
+
+            response: function (res) {
+                cleanCache(res.data['result'] === 'True');
+                return res;
+            },
+
+            responseError: function (res) {
+                return res;
+            }
+        }
+    }
+
+    angular.module('app')
+        .run(run)
+        .factory('testInterceptor', testInterceptor)
+        .config(function ($httpProvider) {
+            $httpProvider.interceptors.push('testInterceptor');
+        });
 }(window.angular));
