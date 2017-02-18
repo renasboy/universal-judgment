ef9e8a1984cf5e21263ae8890235d3247baa7b39
renasboy <renasboy@gmail.com>
2017-02-18 11:48:07 +0100

    Added search to Person and Judgment admins, added sort order to Person admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -6,9 +6,18 @@ class JudgementQualityInline(admin.TabularInline):
     model = models.JudgementQuality
     extra = 1
 
+
 class JudgementAdmin(admin.ModelAdmin):
     inlines = (JudgementQualityInline,)
+    search_fields = ('why',)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+    def get_ordering(self, request):
+        return ('name',)
 
 admin.site.register(models.Quality)
-admin.site.register(models.Person)
+admin.site.register(models.Person, PersonAdmin)
 admin.site.register(models.Judgement, JudgementAdmin)
